33270fe9cbc44bf23170e013ecde2d9717d85d36
Jeff Free <jefffreelance3@gmail.com>
2021-11-27 20:30:04 -0300

    :rocket: fixing deploy stuff

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,7 @@ module.exports = {
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader",
+                    exclude: /node_modules/
                 },
             },
             {
